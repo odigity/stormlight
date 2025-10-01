a8e36a33d4e8b3a069365b59628f07f7635ce13a
Nick Renford <>
2025-10-01 00:14:29 -0400

    improve views and nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import './App.scss'
 import LivePreview from "./pages/LivePreview.tsx";
 import CardDataTable from "./pages/CardDataTable.tsx";
 import {useState} from "react";
+import {AppBar, Box, Button, Container, createTheme, CssBaseline, Stack, ThemeProvider, Toolbar} from "@mui/material";
 
+
+
+const theme = createTheme({
+
+})
 
 function App() {
 
@@ -11,19 +17,33 @@ function App() {
     const handlePageChange = (page) => () => {
         setPage(page)
     };
-    return <div>
-        <div className="navbar">
-            <a href="#" onClick={handlePageChange('data')}>Data</a>
-            &nbsp;|&nbsp;
-            <a href="#" onClick={handlePageChange('livepreview')}>Live Preview</a>
-        </div>
-        {
-            page === "data" && <CardDataTable />
-        }
-        {
-            page === "livepreview" && <LivePreview/>
-        }
-    </div>
+    return <>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AppBar position="static">
+                    <Toolbar disableGutters>
+                        <Stack direction="row" spacing={2} padding={2}>
+                            <Button onClick={handlePageChange('data')} color="inherit" variant="outlined">
+                                Data
+                            </Button>
+                            <Button onClick={handlePageChange('livepreview')} color="inherit" variant="outlined">
+                                Live Preview
+                            </Button>
+                        </Stack>
+                    </Toolbar>
+            </AppBar>
+            <Box sx={{
+                marginTop: 2,
+            }}>
+                {
+                    page === "data" && <CardDataTable />
+                }
+                {
+                    page === "livepreview" && <LivePreview/>
+                }
+            </Box>
+        </ThemeProvider>
+    </>
 }
 
 export default App;
--- src/pages/LivePreview.tsx
+++ src/pages/LivePreview.tsx
@@ -5,6 +5,7 @@ import Card from "../components/Card";
 import {useState} from "react";
 import _ from "lodash";
 import type {CardType} from "../types/cardType.ts";
+import {Box, Button, Grid, Paper, Stack, Typography} from "@mui/material";
 
 
 //TODO: Fucking enums
@@ -23,10 +24,12 @@ const types = [
 
 export default function LivePreview() {
     const [typeFilter, setTypeFilter] = useState<null | CardType>(null);
+    const [sortType, setSortType] = useState<'type' | 'name'>('type');
 
     const cards = _(allCards)
         .sortBy('name')
         .sortBy('type')
+        .sortBy(sortType)
         .filter((card) => {
             return typeFilter
                 ? card.type === typeFilter
@@ -43,17 +46,72 @@ export default function LivePreview() {
         }
     };
 
+    const handleSortChange = (type) => () =>{
+        setSortType(type);
+    }
+
     return (
         <div className="livepreview-container">
-            <div className="quickfilters">
-                {
-                    types.map((type) => {
-                        return <>
-                            <a href="#" onClick={handleFilterChange(type)}>{type}</a>
-                        </>
-                    })
-                }
-            </div>
+
+            <Paper
+                elevation={4}
+                sx={{
+                marginTop: 2,
+                marginBottom: 2,
+                padding: 2,
+            }}>
+                <Typography variant="h4" sx={{marginBottom: 1}}>
+                    Filters (type):
+                </Typography>
+                <Grid container spacing={2}>
+                    {
+                        types.map((type) => {
+                            return <Grid size={2}>
+                                <Button
+                                    key={type}
+                                    onClick={handleFilterChange(type)}
+                                    variant="contained"
+                                    fullWidth
+                                >
+                                    {type}
+                                </Button>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+                <Typography variant="h4">
+                    Sort:
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid size={6}>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            onClick={handleSortChange('type')}
+                        >
+                            Type
+                        </Button>
+                    </Grid>
+                    <Grid size={6}>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            onClick={handleSortChange('name')}
+                        >
+                            Name
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+            {/*<div className="quickfilters">*/}
+            {/*    {*/}
+            {/*        types.map((type) => {*/}
+            {/*            return <>*/}
+            {/*                <a href="#" onClick={handleFilterChange(type)}>{type}</a>*/}
+            {/*            </>*/}
+            {/*        })*/}
+            {/*    }*/}
+            {/*</div>*/}
 
             <div className="flexbox-container">
                 {
